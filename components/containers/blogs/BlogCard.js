bcfd497d91d4eb161aee39be02782ef377738fa1
Jason Eliasen <eliasenjason@gmail.com>
2021-07-20 14:02:14 -0600

    blogs started

--- components/containers/blogs/BlogCard.js
+++ components/containers/blogs/BlogCard.js
@@ -7,7 +7,7 @@ const BlogCard = () => {
                 <Blog.Title>Title</Blog.Title>
                 <Blog.Date>Date</Blog.Date>
                 <Blog.CatchPhrase>Catch Phrase</Blog.CatchPhrase>
-                <Blog.TwitterText>Maecenas malesuada sodales egestas. Integer aliquam ut elit sit amet dictum. Fusce vitae sodales tortor, nec sagittis mauris. Nam id auctor ex, ut laoreet velit. Cras suscipit diam est, et lacinia massa sollicitudin eu. In ullamcorper volutpat magna ut suscipit. Fusce convallis lectus enim, in vestibulum nulla finibus vel.</Blog.TwitterText>
+                <Blog.TwitterText>let this be main Maecenas malesuada sodales egestas. Integer aliquam ut elit sit amet dictum. Fusce vitae sodales tortor, nec sagittis mauris. Nam id auctor ex, ut laoreet velit. Cras suscipit diam est, et lacinia massa sollicitudin eu. In ullamcorper volutpat magna ut suscipit. Fusce convallis lectus enim, in vestibulum nulla finibus vel.</Blog.TwitterText>
                 <Blog.ReadMoreButton>Read More</Blog.ReadMoreButton>
             </Blog.DescriptionContainer>
         </Blog>
